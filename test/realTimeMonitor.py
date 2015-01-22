8025838fc5bbdfdbdcf240cb612c7705decb74c6
LIUYANG <lysc_forever@126.com>
2015-01-22 23:19:13 +0800

    删除冗余文件，修改数据库信息

--- test/realTimeMonitor.py
+++ test/realTimeMonitor.py
@@ -7,7 +7,7 @@ import json
 cameraIDs = ["32040451991311000096","32040451991311000098","32040451991311000099","32040451991311000100","32040451991311000101"] 
 #连接数据库
 conn= psycopg2.connect(host="localhost",port="5432",
-                       user="postgres",password="lysc16978",
+                       user="postgres",password="postgres",
                        dbname="realtime_crowd")
 #最大随机数
 maxNum = 30
